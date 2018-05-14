0f5eacb043cb9342fba21eb5f570fe4a22ca913f
Iratxe <iratxe.campo@opendeusto.es>
2018-05-14 08:47:00 +0200

    .

---
+++ proyecto3/src/proyecto3/VentanaPrincipal.java
@@ -0,0 +1,42 @@
+package proyecto3;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class VentanaPrincipal extends JFrame {
+	public VentanaPrincipal() {
+		setTitle( "Valortel" );
+		setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+		setSize( 640, 400 );
+		setLocationRelativeTo( null );
+		JTextArea n = new JTextArea();
+		JButton bUsuario = new JButton("Introducir usuario");
+		JButton bValorar = new JButton("Valoración de hoteles");
+		JPanel panel = new JPanel();
+		getContentPane().setLayout(new GridLayout(2,1));
+		getContentPane().add(bUsuario);
+		getContentPane().add(bValorar);
+		//getContentPane().add(panel, BorderLayout.CENTER);
+		
+		bUsuario.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				VentanaUsuario vent = new VentanaUsuario();
+				vent.setVisible(true);
+				
+			}
+		});
+			
+		
+	}
+		public static void main(String[] args) {
+			VentanaPrincipal e = new VentanaPrincipal();
+			e.setVisible(true);
+		}
+		
+}
---
+++ proyecto3/src/proyecto3/VentanaUsuario.java
@@ -0,0 +1,24 @@
+package proyecto3;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class VentanaUsuario extends JFrame {
+	public VentanaUsuario() {
+		setTitle( "Valortel" );
+		setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+		setSize( 640, 400 );
+		setLocationRelativeTo( null );
+		JTextField tUsuario = new JTextField();
+		JTextField tContraseña = new JTextField();
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.CENTER);
+		panel.add(tUsuario, BorderLayout.NORTH);
+		panel.add(tContraseña);
+	}
+}
