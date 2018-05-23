f4b73aff3a9ea0cff25b9725ccafcaa9b7bf8d15
Iratxe <iratxe.campo@opendeusto.es>
2018-05-23 08:08:57 +0200

    .

---
+++ proyecto3/src/proyecto3/Login.java
@@ -0,0 +1,19 @@
+package proyecto3;
+
+import javax.swing.*;
+
+
+public class Login extends JFrame {
+		
+	public Login(){
+		JTextField textUsuario =  new JTextField();
+		textUsuario.setHorizontalAlignment(JTextField.CENTER);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
