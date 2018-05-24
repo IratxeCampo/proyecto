246ffdd666c1201d6ae20f4c7f6e3879621d123b
Irati98o <irati98o@10.164.3.8>
2018-05-24 10:22:28 +0200

    hi

---
+++ proyecto3/src/proyecto3/VentanaValoraciones.java
@@ -0,0 +1,69 @@
+package proyecto3;
+
+import java.awt.*;
+import javax.swing.*;
+
+import array.ArrayList;
+
+import java.awt.event.*;
+import java.util.Arrays;
+import java.util.Vector;
+
+public class VentanaValoraciones extends JFrame{
+	
+	public VentanaValoraciones(){
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setSize(1000, 1000);
+		setTitle("Ventana de valoraciones");
+		setLocationRelativeTo(null);
+		String hoteles[] = {"Hilton", "HolidayInn", "Four Seasons" };
+		JComboBox<String> cbHoteles = new JComboBox<String>(hoteles);
+		cbHoteles.setEditable(false);
+		getContentPane().add(cbHoteles, BorderLayout.NORTH);
+		
+		JTextArea taOpiniones = new JTextArea(5, 5);
+		JPanel pSuperior = new JPanel();
+		pSuperior.setLayout(new BoxLayout(pSuperior, BoxLayout.Y_AXIS));
+		JPanel pSup1 = new JPanel();
+		pSup1.add(new JLabel("Seleccione un hotel"));
+		pSup1.add(cbHoteles);
+		pSuperior.add(pSup1);
+		getContentPane().add(pSuperior, BorderLayout.NORTH);
+		getContentPane().add(new JScrollPane(taOpiniones), BorderLayout.CENTER);
+		
+		JList<String> lOpiniones;
+		String[] opis = {"Muy buena comida", "Muy atentos"};
+	    Vector<String> vOpiniones = new Vector<>(Arrays.asList(opis));
+		DefaultListModel<String> mOpiniones = new DefaultListModel<>();
+		for (String n : opis) mOpiniones.addElement(n);
+		vOpiniones = new Vector<>(Arrays.asList(new String[] {"Muy buena comida", "Muy atentos"}));
+		vOpiniones.add("Muy buena comida");
+		lOpiniones = new JList<String>(mOpiniones);
+		
+//		DefaultComboBoxModel<String> datos = new DefaultComboBoxModel<>(hoteles);
+//		cHoteles.setModel(datos);
+//		datos.addElement("Plaza");
+		
+		cbHoteles.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				taOpiniones.setText("Valoraciones: ");
+				taOpiniones.append("\n" + lOpiniones);
+			}
+		});
+	}
+	
+	
+	private void obtenerHoteles(){
+		String lHoteles[];
+		//lHoteles tiene que leerse desde Neo4j
+	}
+
+	
+	public static void main(String[] args){
+		VentanaValoraciones v = new VentanaValoraciones();
+		v.setVisible(true);
+	}
+
+}
